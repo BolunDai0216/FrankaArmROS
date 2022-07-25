7945a0b7d8ddd7921a88a25c0b8f6ba6a6c18741
Bolun Dai <bd1555@nyu.edu>
2022-07-25 16:13:04 -0400

    fixed Could not switch controllers error

--- franka_arm_ros/include/franka_arm_ros/pd_controller.h
+++ franka_arm_ros/include/franka_arm_ros/pd_controller.h
@@ -15,21 +15,39 @@
 #include <franka_hw/franka_model_interface.h>
 #include <franka_hw/trigger_rate.h>
 
+// namespace franka_arm_ros {
+
+// class PDController : public controller_interface::MultiInterfaceController<hardware_interface::EffortJointInterface> {
+//     public:
+//         bool init(hardware_interface::RobotHW* robot_hardware, ros::NodeHandle& node_handle);
+//         // void starting(const ros::Time&) override;
+//         void update(const ros::Time&, const ros::Duration& period);
+
+//     private:
+//         //   hardware_interface::EffortJointInterface* effort_joint_interface_;
+//         //   std::vector<hardware_interface::JointHandle> effort_joint_handles_;
+//         //   ros::Duration elapsed_time_;
+//         //   std::array<double, 7> initial_pose_{};
+//         // std::unique_ptr<franka_hw::FrankaModelHandle> model_handle_;
+//         // std::vector<hardware_interface::JointHandle> joint_handles_;
+// };
+
+// }  // namespace franka_arm_ros
+
+
 namespace franka_arm_ros {
 
-class PDController : public controller_interface::MultiInterfaceController<franka_hw::FrankaModelInterface, hardware_interface::EffortJointInterface> {
-    public:
-        bool init(hardware_interface::RobotHW* robot_hardware, ros::NodeHandle& node_handle) override;
-        void starting(const ros::Time&) override;
-        void update(const ros::Time&, const ros::Duration& period) override;
+class PDController : public controller_interface::MultiInterfaceController<hardware_interface::EffortJointInterface> {
+ public:
+  bool init(hardware_interface::RobotHW* robot_hardware, ros::NodeHandle& node_handle) override;
+  void starting(const ros::Time&) override;
+  void update(const ros::Time&, const ros::Duration& period) override;
 
-    private:
-        //   hardware_interface::EffortJointInterface* effort_joint_interface_;
-        //   std::vector<hardware_interface::JointHandle> effort_joint_handles_;
-        //   ros::Duration elapsed_time_;
-        //   std::array<double, 7> initial_pose_{};
-        std::unique_ptr<franka_hw::FrankaModelHandle> model_handle_;
-        std::vector<hardware_interface::JointHandle> joint_handles_;
+ private:
+  hardware_interface::EffortJointInterface* effort_joint_interface_;
+  std::vector<hardware_interface::JointHandle> effort_joint_handles_;
+  ros::Duration elapsed_time_;
+  std::array<double, 7> initial_pose_{};
 };
 
 }  // namespace franka_arm_ros
--- franka_arm_ros/src/pd_controller.cpp
+++ franka_arm_ros/src/pd_controller.cpp
@@ -1,66 +1,62 @@
+// Copyright (c) 2017 Franka Emika GmbH
+// Use of this source code is governed by the Apache-2.0 license, see LICENSE
 #include <franka_arm_ros/pd_controller.h>
 
 #include <cmath>
-#include <memory>
 
 #include <controller_interface/controller_base.h>
+#include <hardware_interface/hardware_interface.h>
+#include <hardware_interface/joint_command_interface.h>
 #include <pluginlib/class_list_macros.h>
 #include <ros/ros.h>
 
-#include <franka/robot_state.h>
-#include <iostream>
-
 namespace franka_arm_ros {
 
-bool PDController::init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& node_handle) {
-    //checking to see if the default parameters can be access through node handle
-    //and also getting information and interfaces  
-    // std::string arm_id;
-    // if (!node_handle.getParam("arm_id", arm_id)) {
-    //     ROS_ERROR("JointImpedanceController: Could not read parameter arm_id");
-    //     return false;
-    // }
-    // std::vector<std::string> joint_names;
-    // if (!node_handle.getParam("joint_names", joint_names) || joint_names.size() != 7) {
-    //     ROS_ERROR(
-    //         "JointImpedanceController: Invalid or no joint_names parameters provided, aborting "
-    //         "controller init!");
-    //     return false;
-    // }
+bool PDController::init(hardware_interface::RobotHW* robot_hardware,
+                                          ros::NodeHandle& node_handle) {
+  effort_joint_interface_ = robot_hardware->get<hardware_interface::EffortJointInterface>();
+  
+  // check if got effort_joint_interface 
+  if (effort_joint_interface_ == nullptr) {
+    ROS_ERROR(
+        "PDController: Error getting effort joint interface from hardware!");
+    return false;
+  }
+  
+  // check if got joint_names
+  std::vector<std::string> joint_names;
+  if (!node_handle.getParam("joint_names", joint_names)) {
+    ROS_ERROR("PDController: Could not parse joint names");
+  }
+  if (joint_names.size() != 7) {
+    ROS_ERROR_STREAM("PDController: Wrong number of joint names, got " << joint_names.size() << " instead of 7 names!");
+    return false;
+  }
 
-    // double publish_rate(30.0);
-    // if (!node_handle.getParam("publish_rate", publish_rate)) {
-    //     ROS_INFO_STREAM("JointImpedanceController: publish_rate not found. Defaulting to "
-    //                     << publish_rate);
-    // }
-   
-    // auto* effort_joint_interface = robot_hw->get<hardware_interface::EffortJointInterface>();
-    // if (effort_joint_interface == nullptr) {
-    //     ROS_ERROR_STREAM(
-    //         "JointImpedanceController: Error getting effort joint interface from hardware");
-    //     return false;
-    // }
-    // for (size_t i = 0; i < 7; ++i) {
-    //     try {
-    //     joint_handles_.push_back(effort_joint_interface->getHandle(joint_names[i]));
-    //     } catch (const hardware_interface::HardwareInterfaceException& ex) {
-    //     ROS_ERROR_STREAM(
-    //         "JointImpedanceController: Exception getting joint handles: " << ex.what());
-    //     return false;
-    //     }
-    // }
+  // check if joint_handle got each joint
+  effort_joint_handles_.resize(7);
+  for (size_t i = 0; i < 7; ++i) {
+    try {
+      effort_joint_handles_[i] = effort_joint_interface_->getHandle(joint_names[i]);
+    } catch (const hardware_interface::HardwareInterfaceException& e) {
+      ROS_ERROR_STREAM(
+          "PDController: Exception getting joint handles: " << e.what());
+      return false;
+    }
+  }
 
-    return true;
+  return true;
 }
 
-void PDController::starting(const ros::Time& /*time*/) {
-  int i = 0;
+void PDController::starting(const ros::Time& /* time */) {
+    int i = 0;
 }
 
 void PDController::update(const ros::Time& /*time*/, const ros::Duration& period) {
-  int i = 0;
+    int i = 0;
 }
 
 }  // namespace franka_arm_ros
 
-PLUGINLIB_EXPORT_CLASS(franka_arm_ros::PDController, controller_interface::ControllerBase)
+PLUGINLIB_EXPORT_CLASS(franka_arm_ros::PDController,
+                       controller_interface::ControllerBase)
