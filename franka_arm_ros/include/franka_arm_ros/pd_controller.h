ccd4206a47711069874d313ad12acf2b501418c6
Bolun Dai <bd1555@nyu.edu>
2022-08-10 22:51:14 -0400

    added pd tracking controller

--- franka_arm_ros/include/franka_arm_ros/pd_controller.h
+++ franka_arm_ros/include/franka_arm_ros/pd_controller.h
@@ -1,8 +1,17 @@
 #pragma once
 
-#include <pinocchio/parsers/sample-models.hpp>
+#include <pinocchio/fwd.hpp>
+#include <pinocchio/parsers/urdf.hpp>
 #include <pinocchio/algorithm/joint-configuration.hpp>
 #include <pinocchio/algorithm/rnea.hpp>
+#include <pinocchio/algorithm/kinematics.hpp>
+#include <pinocchio/algorithm/jacobian.hpp>
+#include <pinocchio/algorithm/aba.hpp>
+#include <pinocchio/algorithm/rnea.hpp>
+#include <pinocchio/algorithm/crba.hpp>
+#include <pinocchio/algorithm/frames.hpp>
+#include <pinocchio/multibody/model.hpp>
+#include <pinocchio/algorithm/model.hpp>
 
 #include <array>
 #include <string>
@@ -21,6 +30,8 @@
 #include <franka_hw/trigger_rate.h>
 
 #include <Eigen/Dense>
+#include<Eigen/Eigen>
+#include <Eigen/Core>
 
 namespace franka_arm_ros {
 
@@ -34,12 +45,65 @@ class PDController : public controller_interface::MultiInterfaceController<frank
 
  private:
   pinocchio::Model model;
+  pinocchio::Data data;
+
   std::unique_ptr<franka_hw::FrankaStateHandle> state_handle_;
   std::unique_ptr<franka_hw::FrankaModelHandle> model_handle_;
   std::vector<hardware_interface::JointHandle> joint_handles_;
   
   ros::Duration elapsed_time_;
   std::array<double, 7> initial_pose_{};
+
+  int ee_frame_id;
+
+  double kp_delta_q;
+  double kd_delta_dq;
+  double kd_dq;
+
+  double x_0;
+  double y_0;
+  double z_0;
+  double x_T;
+  double y_T;
+  double z_T;
+
+  double terminal_time;
+  double t;
+
+  Eigen::Matrix<double, 6, 6> time_mat;
+
+  Eigen::Matrix<double, 6, 1> x_traj_cond_mat;
+  Eigen::Matrix<double, 6, 1> y_traj_cond_mat;
+  Eigen::Matrix<double, 6, 1> z_traj_cond_mat;
+
+  Eigen::Matrix<double, 6, 1> x_traj_coeff;
+  Eigen::Matrix<double, 6, 1> y_traj_coeff;
+  Eigen::Matrix<double, 6, 1> z_traj_coeff;
+
+  Eigen::Matrix<double, 6, 7> J;
+
+  double x_des;
+  double y_des;
+  double z_des;
+
+  double vx_des;
+  double vy_des;
+  double vz_des;
+
+  double ax_des;
+  double ay_des;
+  double az_des;
+
+  Eigen::Matrix<double, 1, 6> position_time_mat; 
+  Eigen::Matrix<double, 1, 6> velocity_time_mat; 
+  Eigen::Matrix<double, 1, 6> acceleration_time_mat; 
+
+  Eigen::Matrix<double, 3, 1> pos_desired;
+  Eigen::Matrix<double, 3, 1> vel_desired;
+  Eigen::Matrix<double, 3, 1> acc_desired;
+
+  Eigen::Matrix<double, 7, 1> delta_q;
+  Eigen::Matrix<double, 7, 1> delta_dq;
 };
 
 }  // namespace franka_arm_ros
--- franka_arm_ros/src/pd_controller.cpp
+++ franka_arm_ros/src/pd_controller.cpp
@@ -86,28 +86,122 @@ bool PDController::init(hardware_interface::RobotHW* robot_hw,
     }
   }
 
+  // build pin_robot from urdf
+  std::string urdf_filename = "/home/bolun/catkin_ws/src/FrankaArmROS/franka_arm_ros/robots/panda_pin.urdf";
+  pinocchio::urdf::buildModel(urdf_filename, model);
+  data = pinocchio::Data(model);
+
   return true;
 }
 
 void PDController::starting(const ros::Time& /* time */) {
-    int i = 0;
+    // get intial robot state
+    franka::RobotState initial_state = state_handle_->getRobotState();
+
+    // get intial end-effector position
+    Eigen::Affine3d transform(Eigen::Matrix4d::Map(initial_state.O_T_EE.data()));
+    Eigen::Vector3d position(transform.translation());    
+
+    Eigen::Map<Eigen::Matrix<double, 7, 1>> q(initial_state.q.data());
+    Eigen::Map<Eigen::Matrix<double, 7, 1>> dq(initial_state.dq.data());
+    pinocchio::forwardKinematics(model, data, q, dq);
+    pinocchio::updateFramePlacements(model, data);
+
+    ee_frame_id = model.getFrameId("panda_fingertip");
+    
+    // get initial end-effector position
+    x_0 = data.oMf[ee_frame_id].translation()(0);
+    y_0 = data.oMf[ee_frame_id].translation()(1);
+    z_0 = data.oMf[ee_frame_id].translation()(2);
+
+    // get target end-effector position
+    ros::param::get("/x_target", x_T);
+    ros::param::get("/y_target", y_T);
+    ros::param::get("/z_target", z_T);
+    ros::param::get("/terminal_time", terminal_time);
+
+    // get pd gains
+    ros::param::get("/kp_delta_q", kp_delta_q);
+    ros::param::get("/kd_delta_dq", kd_delta_dq);
+    ros::param::get("/kd_dq", kd_dq);
+
+    // solve for trajctory coefficients
+    time_mat << 1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+                0.0, 0.0, 2.0, 0.0, 0.0, 0.0,
+                1.0, terminal_time, pow(terminal_time, 2.0), pow(terminal_time, 3.0), pow(terminal_time, 4.0), pow(terminal_time, 5.0),
+                0.0, 1.0, 2.0*terminal_time, 3.0*pow(terminal_time, 2.0), 4.0*pow(terminal_time, 3.0), 5.0*pow(terminal_time, 4.0),
+                0.0, 0.0, 2.0, 6.0*terminal_time, 12.0*pow(terminal_time, 2.0), 20.0*pow(terminal_time, 3.0);
+    
+    x_traj_cond_mat << x_0, 0.0, 0.0, x_T, 0.0, 0.0; 
+    y_traj_cond_mat << y_0, 0.0, 0.0, y_T, 0.0, 0.0; 
+    z_traj_cond_mat << z_0, 0.0, 0.0, z_T, 0.0, 0.0; 
+
+    x_traj_coeff = time_mat.colPivHouseholderQr().solve(x_traj_cond_mat);
+    y_traj_coeff = time_mat.colPivHouseholderQr().solve(y_traj_cond_mat);
+    z_traj_coeff = time_mat.colPivHouseholderQr().solve(z_traj_cond_mat);
 }
 
 void PDController::update(const ros::Time& /*time*/, const ros::Duration& period) {
-  // get state variables
-  std::array<double, 7> coriolis_array = model_handle_->getCoriolis();
-  Eigen::Map<Eigen::Matrix<double, 7, 1>> coriolis(coriolis_array.data());
-
-  // get target joint angles
-  std::array<double, 7> target_array = {0.5, -0.785398163, 0, -2.35619449, 0, 1.57079632679, 0.785398163397};
-  Eigen::Map<Eigen::Matrix<double, 7, 1>> target(target_array.data());
-
   // get joint angles and angular velocities
   franka::RobotState robot_state = state_handle_->getRobotState();
   Eigen::Map<Eigen::Matrix<double, 7, 1>> q(robot_state.q.data());
   Eigen::Map<Eigen::Matrix<double, 7, 1>> dq(robot_state.dq.data());
 
-  Eigen::Matrix<double, 7, 1> torques = coriolis + 10 * (target - q) - 10 * dq; 
+  // update pinocchio robot model
+  pinocchio::forwardKinematics(model, data, q, dq);
+  pinocchio::updateFramePlacements(model, data);
+
+  // get time
+  t = ros::Time::now().toSec();
+
+  // get state variables
+  std::array<double, 7> coriolis_array = model_handle_->getCoriolis();
+  Eigen::Map<Eigen::Matrix<double, 7, 1>> coriolis(coriolis_array.data());
+
+  // get Jacobian matrix
+  pinocchio::computeFrameJacobian(model, data, q, ee_frame_id, pinocchio::LOCAL_WORLD_ALIGNED, J);
+
+  if (t <= terminal_time) {
+    position_time_mat << 1.0, t, pow(t, 2.0), pow(t, 3.0), pow(t, 4.0), pow(t, 5.0);
+    velocity_time_mat << 0.0, 1.0, 2.0*t, 3.0*pow(t, 2.0), 4.0*pow(t, 3.0), 5.0*pow(t, 4.0);
+    acceleration_time_mat << 0.0, 0.0, 2.0, 6.0*t, 12.0*pow(t, 2.0), 20.0*pow(t, 3.0);
+
+    // get desired position
+    x_des = position_time_mat * x_traj_coeff;
+    y_des = position_time_mat * y_traj_coeff;
+    z_des = position_time_mat * z_traj_coeff;
+    pos_desired << x_des, y_des, z_des;
+    
+    // get desired velocity
+    vx_des = velocity_time_mat * x_traj_coeff;
+    vy_des = velocity_time_mat * y_traj_coeff;
+    vz_des = velocity_time_mat * z_traj_coeff;
+    vel_desired << vx_des, vy_des, vz_des;
+
+    // get desired acceleration
+    ax_des = acceleration_time_mat * x_traj_coeff;
+    ay_des = acceleration_time_mat * y_traj_coeff;
+    az_des = acceleration_time_mat * z_traj_coeff;
+    acc_desired << ax_des, ay_des, az_des;
+  } else {
+    pos_desired << x_T, y_T, z_T;
+    vel_desired << 0.0, 0.0, 0.0;
+    acc_desired << 0.0, 0.0, 0.0;
+  }
+
+  // get errors
+  std::vector<int> row_idx{0, 1, 2};
+  std::vector<int> col_idx{0, 1, 2, 3, 4, 5, 6};
+  auto Jp = J(row_idx, col_idx);
+  delta_q = Jp.colPivHouseholderQr().solve(pos_desired - data.oMf[ee_frame_id].translation());
+  delta_dq = Jp.colPivHouseholderQr().solve(vel_desired) - dq;
+
+  // compute PD controller
+  Eigen::Matrix<double, 7, 1> torques = coriolis + 10 * delta_q + 5 * delta_dq - 0.5 * dq; 
+
+  std::cout << data.oMf[ee_frame_id].translation() << std::endl;
+  std::cout << "*******" << std::endl;
 
   // set torque
   for (size_t i = 0; i < 7; ++i) {
